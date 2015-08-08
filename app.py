03d4670828add51b7120aa79441f2e5a10dd9c9a
Hamfri <kibzham@gmail.com>
2015-08-08 16:38:57 +0300

    Added dynamic routes and set DEBUG to true

--- app.py
+++ app.py
@@ -6,13 +6,44 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
 
 # define the view using a function, which returns a string
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World!?!!?"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print value + 1
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print value + 1.1
+    return "correct"
+
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print value
+    return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
 
 # start the development server using the run() method
 if __name__ == '__main__':
